79062be81e376a2213e90b5847ca7b7f1dbe2bc4
Anne-Cecile Colin <60205617+ACColin@users.noreply.github.com>
2024-05-16 13:09:49 +1000

    change

--- scr/2_script.R
+++
@@ -1,9 +0,0 @@
-headers = c("A", "B", "C", "D", "E")
-random_data = matrix(data = round(runif(20, min = 1, max = 100)), nrow = 4, ncol = 5)
-df = as.data.frame(random_data)
-colnames(df) = headers
-print(df)
-
-write.csv(df, "data/df_1.csv")
-
-
